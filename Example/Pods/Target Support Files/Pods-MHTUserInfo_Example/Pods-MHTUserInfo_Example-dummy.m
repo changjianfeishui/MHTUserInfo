a688e04b6fbb4eb861f15d6e5a599815e38338cc
mangox <329735967@qq.com>
2025-09-30 12:04:13 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-MHTUserInfo_Example/Pods-MHTUserInfo_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MHTUserInfo_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MHTUserInfo_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-MHTUserInfo_Tests/Pods-MHTUserInfo_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MHTUserInfo_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MHTUserInfo_Tests
+@end
